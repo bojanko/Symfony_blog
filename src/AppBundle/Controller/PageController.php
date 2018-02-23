a322a8bd109f84fcb1a404df783449c30df05741
bojanko <31866364+bojanko@users.noreply.github.com>
2018-02-23 02:40:29 +0100

    DB
    Povezivanje baze i mail servera, izmeštanje podataka stranica u bazu

--- src/AppBundle/Controller/PageController.php
+++ src/AppBundle/Controller/PageController.php
@@ -6,10 +6,37 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use AppBundle\Entity\Page;
+
 class PageController extends Controller
 {
     public function homeAction(Request $request)
     {
         return $this->render('home.html.twig', array("page" => "home"));
+    }
+	
+	public function aboutAction(Request $request)
+    {
+		$page = $this->getDoctrine()
+        ->getRepository(get_class(new Page))
+        ->findOneByPage("about");
+		
+        return $this->render('about.html.twig', array("page" => "about", "txt" => $page->getSadrzaj()));
+    }
+	
+	public function contactAction(Request $request)
+    {
+		$page = $this->getDoctrine()
+        ->getRepository(get_class(new Page))
+        ->findOneByPage("contact");
+		
+		/*$message = (new \Swift_Message('Hello Email'))
+        ->setFrom('send@example.com')
+        ->setTo('recipient@example.com')
+        ->setBody("test");
+
+		$this->get('mailer')->send($message);*/
+		
+        return $this->render('contact.html.twig', array("page" => "contact", "txt" => $page->getSadrzaj()));
     }
 }
